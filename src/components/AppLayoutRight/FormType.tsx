84c1afc45c2068a03e7969e9582da2477f8734ad
DragonLJH <1183665002@qq.com>
2022-08-02 17:55:04 +0800

    自定义表单控件

---
+++ src/components/AppLayoutRight/FormType.tsx
@@ -0,0 +1,51 @@
+import { FC, useEffect, useState } from 'react';
+import { Select, Input, InputNumber, } from 'antd';
+const { Option } = Select;
+
+interface FormTypeObj {
+    inputType: string;
+    value?: any;
+    onChange?: any;
+    chidren?: Array<SelectObj>;
+}
+interface SelectObj {
+    value?: string;
+    label?: string;
+}
+
+// 自定义表单控件，根据提供的inputType来对应生成表单控件
+const formTypeConfig: any = {
+    "input": (val: any, set: any) => <Input value={val} onChange={set} />,
+    "inputNumber": (val: any, set: any) => <InputNumber value={val} onChange={set} />,
+    "select": (val: any, set: any, props: Array<SelectObj>) => {
+        return (<Select value={val} onChange={set} >
+            {props.map((val: SelectObj) => {
+                return <Option key={val.value} value={val.value}>{val.label}</Option>
+            })}
+        </Select>)
+    }
+}
+
+// 自定义表单控件
+// 需要 提供受控属性 value，提供 onChange 事件
+// Antd Form.Item 说明
+// 被设置了 name 属性的 Form.Item 包装的控件，表单控件会自动添加 value onChange ，数据同步将被 Form 接管，这会导致以下结果：
+// 1、你不再需要也不应该用 onChange 来做数据收集同步（你可以使用 Form 的 onValuesChange），但还是可以继续监听 onChange 事件。
+// 2、你不能用控件的 value 或 defaultValue 等属性来设置表单域的值，默认值可以用 Form 里的 initialValues 来设置。
+//    注意 initialValues 不能被 setState 动态更新，你需要用 setFieldsValue 来更新。
+// 3、你不应该用 setState，可以使用 form.setFieldsValue 来动态改变表单值。
+const FormType: FC<FormTypeObj> = (props: FormTypeObj) => {
+    const { inputType, chidren, value, onChange } = props  // value 由设置了 name 属性的Form.Item自动添加
+    const [val, setVal] = useState(value)
+    useEffect(() => {
+        setVal(value)
+    })
+    const setFun = (val: any) => {
+        onChange(val)  // onChange 事件 由设置了 name 属性的Form.Item自动添加，修改的值同步到 form
+        setVal(val)
+    }
+    return (formTypeConfig[inputType](val, setFun, chidren))
+}
+
+
+export default FormType;
