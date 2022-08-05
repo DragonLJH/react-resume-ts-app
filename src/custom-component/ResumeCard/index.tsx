1c4f9e17de507e4b48056a1dbbdce8bc3901183a
DragonLJH <1183665002@qq.com>
2022-08-05 16:37:01 +0800

    text（内容）、title（标题）统一使用ResumeText组件代替

--- src/custom-component/ResumeCard/index.tsx
+++ src/custom-component/ResumeCard/index.tsx
@@ -1,15 +1,40 @@
 import React, { FC } from 'react';
 import { Card } from 'antd';
+import ResumeText from "../ResumeText"
+import { IconComponent } from "../../utils/icon-config";
 import './index.css';
+
+const { Meta } = Card
 interface ResumeCardProps {
-    text: string;
-    title?: string;
+    text?: string;
+    title?: any;
+    cover?: any;
+    avatar?: any;
+    icon?: any;
+    secondaryTitle?: string;
+    description?: any;
 }
 const ResumeCard: FC<ResumeCardProps> = (props: ResumeCardProps) => {
-    const { text, title } = props
+    const { text, title, cover, avatar, description, secondaryTitle, icon } = props
+    const mainHead: ResumeCardProps = {}
+    const metaHead: ResumeCardProps = {}
+
+    if (title) mainHead['title'] = <ResumeText {...{ text: title, icon }} />
+    if (cover) mainHead['cover'] = <img src={cover} />
+    if (avatar) metaHead['avatar'] = <IconComponent name={avatar ?? ''} />
+    if (description) metaHead['description'] = <p><ResumeText text={description} /></p>
+
     return (
-        <Card className="ResumeCard" title={title} extra={<a href="#">More</a>}>
-            <p>{text}</p>
+        <Card
+            headStyle={{ height: "20%" }}
+            bodyStyle={{ height: "80%" }}
+            className="ResumeCard" {...mainHead}
+        >
+            {text ? <p><ResumeText {...{ text }} /></p> : ""}
+            <Meta
+                {...metaHead}
+                title={<ResumeText {...{ text: secondaryTitle ?? "" }} />}
+            />
         </Card>
     )
 };
--- src/custom-component/ResumeDivider/index.tsx
+++ src/custom-component/ResumeDivider/index.tsx
@@ -1,24 +1,18 @@
-import React, { FC } from 'react';
-import iconConfig from "../../utils/icon-config";
+import React, { FC } from 'react'; 
+import ResumeText from "../ResumeText"
 import { Divider } from 'antd';
 import './index.css';
 interface ResumeDividerProps {
     text: string;
     orientation?: "left" | "right" | "center" | undefined;
     type?: "horizontal" | "vertical" | undefined;
-    icon?: string;
+    icon: string;
 }
 const ResumeDivider: FC<ResumeDividerProps> = (props: ResumeDividerProps) => {
     const { text, orientation, type, icon } = props
-    const IconComponent = () => {
-        if (icon) {
-            return <span style={{ padding: "0px 5px" }}>{iconConfig[icon]()}</span>
-        }
-        return <span></span>
-    }
     return (
         <Divider type={type} orientation={orientation} className="ResumeDivider">
-            <IconComponent />  {text}
+            <ResumeText {...{ text, icon }} />
         </Divider>
     )
 };
