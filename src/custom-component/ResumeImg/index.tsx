a5dc51b0224046eefec28cfb3f403b50ca640296
DragonLJH <1183665002@qq.com>
2022-08-05 16:40:51 +0800

    添加属性控制（样式控制）图片呈现（方圆）

--- src/custom-component/ResumeImg/index.tsx
+++ src/custom-component/ResumeImg/index.tsx
@@ -3,11 +3,12 @@ import { Image } from 'antd';
 import './index.css';
 interface ResumeImgProps {
     src: string;
+    circle: boolean;
 }
 const ResumeImg: FC<ResumeImgProps> = (props: ResumeImgProps) => {
-    const { src } = props
+    const { src, circle } = props
     return (
-        <Image rootClassName="rootResumeImg" className="ResumeImg" src={src} fallback="http://150.158.96.29:8082/commom-img/error.png" preview={false}  />
+        <Image rootClassName="rootResumeImg" className={`ResumeImg ${circle ? 'circle' : ''}`} src={src} fallback="http://150.158.96.29:8082/commom-img/error.png" preview={false} />
     )
 };
 
