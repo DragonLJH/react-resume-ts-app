109af3c141d494efb3cb230e0b8fd238a8594813
DragonLJH <1183665002@qq.com>
2022-08-05 16:34:01 +0800

    使内容连续不再被sapn一个个分离

--- src/custom-component/ResumeText/index.tsx
+++ src/custom-component/ResumeText/index.tsx
@@ -1,29 +1,41 @@
 import React, { FC } from 'react';
-import iconConfig from "../../utils/icon-config";
+import { IconComponent } from "../../utils/icon-config";
 import './index.css';
 interface ResumeTextProps {
     text: string;
-    icon: string;
+    icon?: string;
 }
 const ResumeText: FC<ResumeTextProps> = (props: ResumeTextProps) => {
     const { text, icon } = props
-    const IconComponent = () => {
-        if (icon) {
-            return <span style={{ padding: "0px 5px" }}>{iconConfig[icon]()}</span>
-        }
-        return <span></span>
-    }
-    // 识别空格符和换行符
+    // 识别空格符和换行符 
     const TextComponent = (): any => {
-        return text.split("").map((val, index) => {
-            if (val === " ") return <span key={index}>&nbsp;</span>
+        let flagText = text.split("")
+        let newText = []
+        let flag = ""
+        for (let i = 0; i < flagText.length; i++) { //使内容连续不再被sapn一个个分离
+            if (flagText[i] === " ") {
+                flag = ""
+                newText.push("&nbsp;", "")
+                continue
+            }
+            if (flagText[i] === "\n") {
+                flag = ""
+                newText.push("\n", "")
+                continue
+            }
+            flag = flag + flagText[i]
+            newText.splice(-1, 1, flag)
+        }
+
+        return newText.map((val, index) => {
+            if (val === "&nbsp;") return <span key={index}>&nbsp;</span>
             if (val === "\n") return <br key={index} />
             return <span key={index}>{val}</span>
         })
     }
     return (
         <div className="ResumeText">
-            <IconComponent /><TextComponent />
+            <IconComponent name={icon ?? ""} /><TextComponent />
         </div>
     )
 };
